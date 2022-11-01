e231fb41590b292eaeb410c2142820b3b5db0969
m.ranjith <mranjith0808@gmail.com>
2022-11-01 06:43:53 +0530

    Initial code updated

---
+++ src/Components/Batch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from '../Image/reactlogo.jpg'
+function Batch()
+{
+    return(
+        <div className="bg-primary">
+          <div>
+          <img className="logo" src={Logo} alt="logo"/>
+        </div>
+        <div>
+          <h1>hello react programing</h1>
+        </div>
+        </div>
+   )
+    }
+    export default Batch
